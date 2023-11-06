08b205dcd696f7ee93d560c58d0a732343aee1b9
RoksolianaKeda <149332889+RoksolianaKeda@users.noreply.github.com>
2023-11-06 21:28:50 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -5,33 +5,41 @@ def validate_board(board: list)-> bool:
     >>> validate_board(["**** ****//n", "***1 ****//n", "**  3****//n", "* 4 1****//n",\
 "     9 5 //n", " 6  83  *//n", "3   1  **//n", "  8  2***//n", "  2  ****//n"])
     False
+    >>> validate_board(["*  *  *  //n", "* 1* *  * //n", "* * 3* * *//n", " 4* 1* * *//n",\
+"     9 5 //n", " 6  8*3  *//n", "3   1* * * //n", "  8  2*** //n", "  2  * * * //n"])
+    True
     '''
-    for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                digits = set()
-                for k in range(3):
-                    for l in range(3):
-                        cell = board[i + k][j + l]
-                        if cell == ' ':
-                            continue
-                        if cell in digits:
-                            return False
-                        digits.add(cell)
     for i in range(9):
-        row_digits = set()
-        col_digits = set()
+        digits = set()
         for j in range(9):
-            row_square = board[i][j]
-            col_squere = board[j][i]
-
-            if row_square == ' ' or col_squere == ' ':
-                continue
-
-            if row_square in row_digits or col_squere in col_digits:
+            cell = board[i][j]
+            if cell in digits or (cell.isdigit() and int(cell) > 0 and int(cell) <= 9):
+                digits.add(cell)
+            elif cell != ' ' and cell != '*':
                 return False
 
-            row_digits.add(row_square)
-            col_digits.add(col_squere)
+    # Validate columns
+    for i in range(9):
+        digits = set()
+        for j in range(9):
+            cell = board[j][i]
+            if cell in digits or (cell.isdigit() and int(cell) > 0 and int(cell) <= 9):
+                digits.add(cell)
+            elif cell != ' ' and cell != '*':
+                return False
+
+    # Validate blocks
+    for block_row in range(0, 9, 3):
+        for block_col in range(0, 9, 3):
+            digits = set()
+            for i in range(3):
+                for j in range(3):
+                    cell = board[block_row + i][block_col + j]
+                    if cell in digits or (cell.isdigit() and int(cell) > 0 and int(cell) <= 9):
+                        digits.add(cell)
+                    elif cell != ' ' and cell != '*':
+                        return False
+
     return True
 
 if __name__ == "__main__":
