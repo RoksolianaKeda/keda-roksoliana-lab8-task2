6b33734c4fbdf65159ff5a06eb2f35f1c4828279
RoksolianaKeda <149332889+RoksolianaKeda@users.noreply.github.com>
2023-11-06 21:31:31 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,43 +1,42 @@
 ''' Puzzle '''
-def validate_board(board: list)-> bool:
+def validate_board(board):
     '''
-    Function to set whether the logic puzzle board is ready to start playing
+    function to set whether the logic puzzle board is ready to start playing
     >>> validate_board(["**** ****//n", "***1 ****//n", "**  3****//n", "* 4 1****//n",\
 "     9 5 //n", " 6  83  *//n", "3   1  **//n", "  8  2***//n", "  2  ****//n"])
     False
-    >>> validate_board(["*  *  *  //n", "* 1* *  * //n", "* * 3* * *//n", " 4* 1* * *//n",\
-"     9 5 //n", " 6  8*3  *//n", "3   1* * * //n", "  8  2*** //n", "  2  * * * //n"])
-    True
     '''
     for i in range(9):
-        digits = set()
+        row_counts = {}
+        col_counts = {}
         for j in range(9):
-            cell = board[i][j]
-            if cell in digits or (cell.isdigit() and int(cell) > 0 and int(cell) <= 9):
-                digits.add(cell)
-            elif cell != ' ' and cell != '*':
-                return False
+            row_digit = board[i][j]
+            column_digit = board[j][i]
 
-    # Validate columns
-    for i in range(9):
-        digits = set()
-        for j in range(9):
-            cell = board[j][i]
-            if cell in digits or (cell.isdigit() and int(cell) > 0 and int(cell) <= 9):
-                digits.add(cell)
-            elif cell != ' ' and cell != '*':
-                return False
+            if row_digit != ' ':
+                if row_digit in row_counts:
+                    return False
+                row_counts[row_digit] = 1
 
-    # Validate blocks
-    for block_row in range(0, 9, 3):
-        for block_col in range(0, 9, 3):
-            digits = set()
-            for i in range(3):
-                for j in range(3):
-                    cell = board[block_row + i][block_col + j]
-                    if cell in digits or (cell.isdigit() and int(cell) > 0 and int(cell) <= 9):
-                        digits.add(cell)
-                    elif cell != ' ' and cell != '*':
+            if column_digit != ' ':
+                if column_digit in col_counts:
+                    return False
+                col_counts[column_digit] = 1
+
+    for color in range(1, 10):
+        result = []
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == str(color):
+                    result.append((i, j))
+
+        if len(result) != 9:
+            return False
+
+        for i in range(9):
+            for j in range(9):
+                if (i, j) not in result:
+                    if board[i][j] != ' ':
                         return False
 
     return True
